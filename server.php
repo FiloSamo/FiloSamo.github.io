c40c4774f8f24a41bc65a11bcbc755e1b42dd671
FiloSamo <48647121+FiloSamo@users.noreply.github.com>
2019-06-14 17:00:55 +0200

    Add files via upload

--- server.php
+++ server.php
@@ -2,10 +2,28 @@
    if($_POST)
    {
        $informazione = $_POST["dato"];
-
    }
-
- header('Content-Type : application/json');
-  $dato = $informazione;
-  print json_encode($dato);
 ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>atleta</title>
+  </head>
+  <body>
+    <div class="Contenitore">
+      <div class="Finestra">
+        <h1>ATTENDI DI PARTIRE</h1>
+      </div>
+      <?php if ($informazione == 'A'): ?>
+        <p>ai vostri posti</p>
+      <?php else if ($informazione == 'B'): ?>
+        <p>pronti</p>
+      <?php else if ($informazione == 'C'): ?>
+        <p>via</p>
+        <?php ?>
+        <?php else: ?>
+      <?php endif ?>
+    </div>
+  </body>
+</html>
